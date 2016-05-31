5844b56e492f641ca8b190ef6ee6c25ef385edd4
Jakob Skjerning <jakob@mentalized.net>
2016-05-31 21:26:16 +0200

    Output estimate for each task

--- estimator/renderer.rb
+++ estimator/renderer.rb
@@ -17,12 +17,37 @@ class Renderer
 
   private
 
+  def indent(name, level)
+    return name if root?(level)
+    indent = "  " * (level - 1) + "* "
+    indent + name
+  end
+
   def output_task(task, level = 0)
-    indent = "  " * level
-    puts indent + task.name
+    indented_name = indent(task.name, level)
+
+    puts format(
+      "%-40s %3i %3i",
+      indented_name,
+      task.min_hours.round,
+      task.max_hours.round
+    )
+
+    if root?(level)
+      puts [
+        "-" * 40,
+        "-" * 3,
+        "-" * 3
+      ].join("-")
+    end
     task.tasks.each do |sub_task|
       output_task(sub_task, level + 1)
     end
-    puts if level.zero?
+
+    puts if root?(level)
+  end
+
+  def root?(level)
+    level.zero?
   end
 end
