c593d0378cc491263a394245d2dbf93810386fbb
Jakob Skjerning <jakob@mentalized.net>
2018-08-24 11:30:29 +0200

    Make it possible to calculate number of iterations

--- estimator/estimate.rb
+++ estimator/estimate.rb
@@ -3,6 +3,7 @@ require_relative "task_list"
 
 class Estimate
   attr_accessor \
+    :days_per_iteration,
     :hours_per_day,
     :price_per_day
 
@@ -19,10 +20,27 @@ class Estimate
     )
   end
 
+  def hours_to_days(hours)
+    hours.to_f / hours_per_day
+  end
+
+  def hours_to_iterations(hours)
+    days = hours_to_days(hours)
+    (days.to_f / days_per_iteration).ceil
+  end
+
   def hours
     Range.new(
       min_hours,
       max_hours
+    )
+  end
+
+  def iterations
+    return nil unless days_per_iteration
+    Range.new(
+      hours_to_iterations(hours.first),
+      hours_to_iterations(hours.last)
     )
   end
 
