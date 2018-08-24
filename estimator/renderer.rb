1ffeecf2743aba99d0816c30185a83f2d10f9193
Jakob Skjerning <jakob@mentalized.net>
2018-08-24 11:31:18 +0200

    Introduce aggregate rows with total hours/days/iterations

--- estimator/renderer.rb
+++ estimator/renderer.rb
@@ -8,12 +8,25 @@ class Renderer
   end
 
   def output
+    puts format("%-40s %13s  %13s  %13s", "Estimate", "Hours", "Days", "Iterations")
+    puts
+
     estimate.tasks.each do |task|
       output_task(task)
     end
 
-    puts format_row("Total hours", estimate.hours.first, estimate.hours.last)
-    puts format_row("Total days", estimate.days.first, estimate.days.last)
+    puts format("%-40s %13s  %13s  %13s", "", "Hours", "Days", "Iterations")
+    puts format_aggregate_row(
+      "Total",
+      estimate.hours.first,
+      estimate.hours.last,
+      estimate.days.first,
+      estimate.days.last,
+      estimate.iterations.first,
+      estimate.iterations.last
+      )
+    puts
+
     puts format("%-40s %6i %6i", "Total price", estimate.price.first, estimate.price.last)
   end
 
@@ -22,6 +35,13 @@ class Renderer
   def format_row(*args)
     format(
       "%-40s %6.0f %6.0f",
+      *args
+    )
+  end
+
+  def format_aggregate_row(*args)
+    format(
+      "%-40s %6.0f %6.0f  %6.0f %6.0f  %6.0f %6.0f",
       *args
     )
   end
@@ -35,12 +55,26 @@ class Renderer
   def output_task(task, level = 0)
     indented_name = indent(task.name, level)
 
-    row = format_row(
-      indented_name,
-      task.min_hours.round(1),
-      task.max_hours.round(1)
-    )
-    row = row.white if root?(level)
+    row = if root?(level)
+      min_hours = task.min_hours
+      max_hours = task.max_hours
+
+      format_aggregate_row(
+        indented_name,
+        min_hours.round(1),
+        max_hours.round(1),
+        estimate.hours_to_days(min_hours),
+        estimate.hours_to_days(max_hours),
+        estimate.hours_to_iterations(min_hours),
+        estimate.hours_to_iterations(max_hours)
+      ).white + "\n"
+    else
+      format_row(
+        indented_name,
+        task.min_hours.round(1),
+        task.max_hours.round(1)
+      )
+    end
     puts row
 
     task.tasks.each do |sub_task|
