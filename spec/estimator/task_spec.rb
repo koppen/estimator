6933dd54a3bbe81f542c07493aed7ce5d19e5ede
Jakob Skjerning <jakob@mentalized.net>
2020-08-10 16:02:51 +0200

    Characterization tets for Task#min_hours

--- spec/estimator/task_spec.rb
+++ spec/estimator/task_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe Task do
       described_class.new("Name of the task", 12, 23)
     ).to be_instance_of(described_class)
   end
+
+  describe "#min_hours" do
+    context "has child tasks" do
+      before do
+        subject.add_task(Task.new("Something", 2, 4))
+        subject.add_task(Task.new("Something else", 3, 5))
+      end
+
+      it "returns the sum of min_hours from child tasks" do
+        expect(subject.min_hours).to eq(2 + 3)
+      end
+    end
+
+    context "has no child tasks" do
+      it "returns its own min_hours" do
+        expect(subject.min_hours).to eq(12)
+      end
+    end
+  end
 end
