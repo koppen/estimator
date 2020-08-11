c03d8eac615e0cd7f9a3477fb64d46f79f8cce38
Jakob Skjerning <jakob@mentalized.net>
2020-08-11 09:03:58 +0200

    Spec tasks and sub tasks

--- spec/estimator/estimate_spec.rb
+++ spec/estimator/estimate_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe Estimate do
       described_class.new
     ).to be_instance_of(described_class)
   end
+
+  it "has tasks" do
+    estimate = Estimate.new
+    estimate.build do |e|
+      e.task("Stuff", 5, 6)
+      e.task("More work", 10, 20)
+    end
+
+    expect(estimate.hours).to eq(Range.new(15, 26))
+  end
+
+  it "has sub-tasks" do
+    estimate = Estimate.new
+    estimate.build do |e|
+      e.task("Complex") do |t|
+        t.task("Detailed work", 10, 20)
+      end
+    end
+
+    expect(estimate.hours).to eq(Range.new(10, 20))
+  end
 end
